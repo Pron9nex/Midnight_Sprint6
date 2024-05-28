f8a28714520eb02dd4c71d728055633140a0164d
Никита Пронин <pron9nex@MacBook-Air-Nikita.local>
2024-05-28 23:18:38 +0300

    Le Chance Finale v2.0.0

--- precode.go
+++ precode.go
@@ -1,15 +1,16 @@
 package main
 
-//Push 1.0.1
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/go-chi/chi/v5"
+	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
-// Task ...
+// Task
 type Task struct {
 	ID           string   `json:"id"`
 	Description  string   `json:"description"`
@@ -46,31 +47,51 @@ var tasks = map[string]Task{
 func getTasks(w http.ResponseWriter, r *http.Request) {
 	resp, err := json.Marshal(tasks)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalf("error marshal tasks %v", err)
+		http.Error(w, "error marshal tasks", http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+	_, err = w.Write(resp)
+
+	if err != nil {
+		log.Fatalf("error writing response: %v", err)
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 // Пишем 2 обработчик (для отправки задачи на сервер)
-
 func postTask(w http.ResponseWriter, r *http.Request) {
 	var task Task
 	var buf bytes.Buffer
 
 	_, err := buf.ReadFrom(r.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("error reading body: %v", err)
+		http.Error(w, "error reading body", http.StatusInternalServerError)
 		return
 	}
+
 	if err := json.Unmarshal(buf.Bytes(), &task); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("error unmarshalling body: %v", err)
+		http.Error(w, "error reading body", http.StatusInternalServerError)
+		return
 	}
+	for _, t := range tasks {
+		if t.ID == task.ID {
+			log.Fatalf("task %v already exists", t.ID)
+			http.Error(w, "task already exists", http.StatusBadRequest)
+			return
+		}
+	}
+
 	tasks[task.ID] = task
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 }
 
 // Пишем 3 обработчки для получения задачи по ID
@@ -78,17 +99,24 @@ func getTask(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	task, ok := tasks[id]
 	if !ok {
+		log.Fatalf("task %s not found", id)
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
 	resp, err := json.Marshal(task)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("error marshal task %v", err)
+		http.Error(w, "error marshal task", http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Fatalf("error writing response: %v", err)
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+		return
+	}
 }
 
 // Пишем 4 обработчик удаления задачи по ID
@@ -100,13 +128,18 @@ func delTask(w http.ResponseWriter, r *http.Request) {
 	}
 	resp, err := json.Marshal(tasks)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	delete(tasks, id)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Fatalf("error writing response: %v", err)
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
